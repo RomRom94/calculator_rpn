36b6b86e032ffa5fd106430f5f0d290ba99c326c
Romain <ferjuleromain@gmail.com>
2019-11-22 13:45:27 +0100

    Init calculator

--- src/App.js
+++ src/App.js
@@ -1,26 +1,97 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    stack: [0, 0, 0, 0, 0, 0, 0, 0, 0]
+  }
+
+  render() {
+    return (
+      <div className="App" >
+        <div className="container">
+          <table class="calculatrice" id="calc">
+            <tr>
+              <td colspan="4" class="calc_td_resultat">
+                <input type="text" readonly="readonly" name="calc_resultat" id="calc_resultat" class="calc_resultat"
+                  onkeydown="javascript:key_detect_calc('calc',event);" />
+              </td>
+            </tr>
+            <tr>
+              <td class="calc_td_btn">
+                <input type="button" class="calc_btn" value="CE" />
+              </td>
+              <td class="calc_td_btn">
+                <input type="button" class="calc_btn" value="&larr;" />
+              </td>
+              <td class="calc_td_btn">
+                <input type="button" class="calc_btn" value="%" />
+              </td>
+              <td class="calc_td_btn">
+                <input type="button" class="calc_btn" value="+" />
+              </td>
+            </tr>
+            <tr>
+              <td class="calc_td_btn">
+                <input type="button" class="calc_btn" value="7" />
+              </td>
+              <td class="calc_td_btn">
+                <input type="button" class="calc_btn" value="8" />
+              </td>
+              <td class="calc_td_btn">
+                <input type="button" class="calc_btn" value="9" />
+              </td>
+              <td class="calc_td_btn">
+                <input type="button" class="calc_btn" value="-" />
+              </td>
+            </tr>
+            <tr>
+              <td class="calc_td_btn">
+                <input type="button" class="calc_btn" value="4" />
+              </td>
+              <td class="calc_td_btn">
+                <input type="button" class="calc_btn" value="5" />
+              </td>
+              <td class="calc_td_btn">
+                <input type="button" class="calc_btn" value="6" />
+              </td>
+              <td class="calc_td_btn">
+                <input type="button" class="calc_btn" value="x" />
+              </td>
+            </tr>
+            <tr>
+              <td class="calc_td_btn">
+                <input type="button" class="calc_btn" value="1" />
+              </td>
+              <td class="calc_td_btn">
+                <input type="button" class="calc_btn" value="2" />
+              </td>
+              <td class="calc_td_btn">
+                <input type="button" class="calc_btn" value="3" />
+              </td>
+              <td class="calc_td_btn">
+                <input type="button" class="calc_btn" value="&divide;" />
+              </td>
+            </tr>
+            <tr>
+              <td class="calc_td_btn">
+                <input type="button" class="calc_btn" value="0" />
+              </td>
+              <td class="calc_td_btn">
+                <input type="button" class="calc_btn" value="&plusmn;" />
+              </td>
+              <td class="calc_td_btn">
+                <input type="button" class="calc_btn" value="," />
+              </td>
+              <td class="calc_td_btn">
+                <input type="button" class="calc_btn" value="=" />
+              </td>
+            </tr>
+          </table>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
